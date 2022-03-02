1645d37846792c9f3b7e059ccf7a11b74ef88227
Riaan Nolan <riaan.nolan@servian.com>
2022-03-02 04:19:43 +0000

    Merge branch 'fix/gregluxford/terraform' into 'master'
    Fix/gregluxford/terraform
    
    See merge request all-staff/hashiqube!76

--- localstack/main.tf
+++ localstack/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
   region                      = "us-east-1"
-  s3_force_path_style         = true
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
@@ -41,6 +41,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "test-bucket" {
   bucket = "my-bucket"
+}
+
+resource "aws_s3_bucket_acl" "test-bucket-acl" {
+  bucket = aws_s3_bucket.test-bucket.id
   acl    = "private"
 }
 
