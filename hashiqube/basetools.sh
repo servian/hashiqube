47457a58149658d716a558348c388ef79ada5b4d
Riaan Nolan <riaan.nolan@servian.com>
2022-10-27 01:43:15 +0000

    Merge branch 'feature/issue-16-ansible-tower-uplift' into 'master'
    Feature/issue 16 ansible tower uplift
    
    See merge request all-staff/hashiqube!122

--- hashiqube/basetools.sh
+++ hashiqube/basetools.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 export PATH=$PATH:/root/.local/bin
 sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update -o Acquire::CompressionTypes::Order::=gz
 sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes upgrade
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install swapspace rkhunter jq curl unzip software-properties-common bzip2 git make python3.9 python3-pip python3-dev python3-virtualenv golang-go apt-utils ntp update-motd toilet figlet nano iputils-ping dnsutils iptables
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install swapspace rkhunter jq curl unzip software-properties-common bzip2 git make python3.9 python3-pip python3-dev python3-virtualenv golang-go apt-utils ntp update-motd toilet figlet nano iputils-ping dnsutils iptables telnet
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1 --force
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1 --force
 python -V
